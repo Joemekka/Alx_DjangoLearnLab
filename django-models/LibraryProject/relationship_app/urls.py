89f3d235d0f536a0ef73e0e08ec26f9174564948
Joemekka <mekka988@gmail.com>
2026-02-08 22:52:19 +0100

    ddd

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,12 +1,20 @@
 from django.urls import path
-from . import views
-from .views import list_books
+from django.urls import path
+from .views import list_books, LibraryDetailView
 
 
 urlpatterns = [
     path("books/", views.list_books, name="list_books"),  # FBV
     path("library/<int:pk>/", views.LibraryDetailView.as_view(), name="library_detail"),
-    path("register/", views.register_view, name="register"),
-    path("login/", views.login_view, name="login"),
-    path("logout/", views.logout_view, name="logout"),
+    path(
+        "login/",
+        LoginView.as_view(template_name="relationship_app/login.html"),
+        name="login",
+    ),
+    path(
+        "logout/",
+        LogoutView.as_view(template_name="relationship_app/logout.html"),
+        name="logout",
+    ),
+    path("register/", register, name="register"),
 ]
