79848d2479bed218e50fa87bc629308608e673e0
Joemekka <mekka988@gmail.com>
2026-02-08 22:38:59 +0100

    fff

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from .models import Library
 from .models import Book
 from django.views.generic import DetailView
+from django.views.generic.detail import DetailView
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.forms import AuthenticationForm
