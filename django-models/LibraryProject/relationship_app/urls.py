2b7e3358281168d35aa8913ae08e423ebcad9892
Joemekka <mekka988@gmail.com>
2026-02-09 08:37:46 +0100

    djfdh

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -34,7 +34,7 @@ urlpatterns = [
     path("librarian/", librarian_view, name="librarian_view"),
     path("member/", member_view, name="member_view"),
     # ===== Task 4: Custom Permissions =====
-    path("book/add/", add_book, name="add_book"),
+    path("add_book/edit_book", add_book, name="add_book"),
     path("book/edit/<int:book_id>/", edit_book, name="edit_book"),
     path("book/delete/<int:book_id>/", delete_book, name="delete_book"),
 ]
