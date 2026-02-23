84f3c987c38cc619c12e213ca8be366c45f4f190
Joemekka <mekka988@gmail.com>
2026-02-23 04:49:38 +0100

    path fixed

--- social_media_api/social_media_api/urls.py
+++ social_media_api/social_media_api/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("accounts/", include("accounts.urls")),
-    path("posts/", include("posts.urls")),
-    path("notifications/", include("notifications.urls")),
+    path("api/accounts/", include("accounts.urls")),
+    path("api/posts/", include("posts.urls")),
+    path("api/notifications/", include("notifications.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
